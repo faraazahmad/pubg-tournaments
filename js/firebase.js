adea48119a38faba72091b38b4e0142fa2c4cd93
Syed Faraaz Ahmad <faraaz98@live.com>
2018-08-07 18:24:57 +0530

    finish basic layout

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    /* IMPORTANT: Use only as environment variable */
+    apiKey: "AIzaSyASHadNA5KCvdl-GTXRbDJuEBLryuhdX04",
+    authDomain: "pubg-tournament.firebaseapp.com",
+    databaseURL: "https://pubg-tournament.firebaseio.com",
+    projectId: "pubg-tournament",
+    storageBucket: "",
+    messagingSenderId: "568585129544"
+};
+firebase.initializeApp(config);
